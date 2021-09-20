3735f273f32b9b7f038a3137e949f7bd40d691ad
Serg901 <sergeylaz903@gmail.com>
2021-09-20 22:06:15 +1000

    remove

--- src/constants/multicall/index.ts
+++
@@ -1,9 +0,0 @@
-import { ChainId } from 'jetswap-sdk-new'
-import MULTICALL_ABI from './abi.json'
-
-const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
-  [ChainId.MAINNET]: '0x92bB764BFb54d0c3Cba333d5D59cAbE9174b7D77', // TODO
-  [ChainId.BSCTESTNET]: '0x301907b5835a2d723Fe3e9E8C5Bc5375d5c1236A'
-}
-
-export { MULTICALL_ABI, MULTICALL_NETWORKS }
